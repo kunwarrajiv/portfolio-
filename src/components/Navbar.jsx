d19836ab684461b2ad9a72bf1ebd2e1009993251
kunwarrajiv <38578063+kunwarrajiv@users.noreply.github.com>
2024-08-16 17:54:42 +0100

    updated the title for page
    updated the title for page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from '../assets/kevinRushLogo.png';
+import logo from '../assets/KunwarRajivLogo.png';
 import { FaLinkedin } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
